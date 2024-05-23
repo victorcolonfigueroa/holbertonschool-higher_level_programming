cf8c7496c72bcba56085fc18a8d555b2b444249a
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-05-23 09:00:58 -0400

    task 5

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Geometry module"""
+
+
 class BaseGeometry:
-    """
-    This is the base class for geometry objects.
-    """
+    """empty class BaseGeometry"""
+
     pass
