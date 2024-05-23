fe7ff7ce5d3f11ec682eec7f922e4db6dbe37bc2
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-05-23 08:43:17 -0400

    fixed pycodestyle o task 3 adn added comment to task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Only sub class of"""
+
+
 def inherits_from(obj, a_class):
-    if (type(obj) != a_class):
-        return issubclass(type(obj), a_class)
-    return False
+    """checks if an object is an instance of a class that inherited
+    (directly or indirectly) from the specified class"""
+
+    return (type(obj) is not a_class and issubclass(type(obj), a_class))
