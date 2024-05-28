1dc40d1206bfd0854ea20ec1d4e9b8d0021dffe7
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-05-28 17:52:34 -0400

    quick fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, encoding='utf-8') as fd:
         for line in fd:
             print(f.read(), end='')
